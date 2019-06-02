caf68bdbc0072626a27c0086d9e4cea4ac0b75ce
dev <honeydevp@gmail.com>
2019-06-02 23:26:04 +0530

    Setting Title4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import NavMain from "./Components/NavBar/NavMain";
 
 const RouterItem = lazy(() => import("./Components/Routes/RouterItem"));
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Dev's Page";
+  });
+
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
